eea83e90978b3ffdebcf21b827be8b63ce48142f
iwangrizzlee <49709695+iwangrizzlee@users.noreply.github.com>
2019-04-18 11:51:01 +0600

    v

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1555566057,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/фон-sheet0.png",
+		"images/ставить-sheet0.png",
+		"images/машинамайнинга-sheet0.png",
+		"images/машинамайнинга-sheet1.png",
+		"images/добавлено1или5или10-sheet0.png",
+		"images/добавлено1или5или10-sheet1.png",
+		"images/добавлено1или5или10-sheet2.png",
+		"images/фон2-sheet0.png",
+		"images/менюкнпкпоставить-sheet0.png",
+		"images/кнпкпоставить-sheet0.png",
+		"images/кнпккупить-sheet0.png",
+		"images/кнпквмагазилидооой-sheet0.png",
+		"images/кнпквмагазилидооой-sheet1.png",
+		"images/cam-sheet0.png",
+		"images/save-sheet0.png",
+		"images/машинамайнинга2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"xd_connection.js"
+	]
+}
