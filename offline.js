e08d4c25d1ecfa4c744df07173e398dbbb507dda
iwangrizzlee <49709695+iwangrizzlee@users.noreply.github.com>
2019-04-18 20:01:08 +0600

    k

--- offline.js
+++ offline.js
@@ -1,10 +1,11 @@
 ﻿{
-	"version": 1555595818,
+	"version": 1555596062,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
+		"images/sprite-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
